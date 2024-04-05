e2fffc4009f571cb459c1ba0f2b23636c211f66c
IanLopezYubero <47207551+IanLopezYubero@users.noreply.github.com>
2024-04-05 10:42:48 +0000

    subida

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar/calendar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CalendarComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
