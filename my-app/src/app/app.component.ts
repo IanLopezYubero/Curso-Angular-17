46dc60ce655f6fca02462855ade5009e762b365d
IanLopezYubero <47207551+IanLopezYubero@users.noreply.github.com>
2024-04-09 06:13:29 +0000

    menu

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar/calendar.component';
 import { CounterComponent } from './counter/counter.component';
+import { TimerComponent } from './timer/timer.component';
+import { LayooutComponent } from './pages/layoout/layoout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CalendarComponent, CounterComponent],
+  imports:[
+    CalendarComponent,
+    CounterComponent,
+    TimerComponent,
+    LayooutComponent
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  name = 'pedro';
-  user = "Usuario";
-  data = [1,2,3,4,5,6];
+  name = 'Pedro';
+  user = undefined;
+  data=[1,2,3,4,5,6]
 }
